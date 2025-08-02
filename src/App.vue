6fd1a660503fa0b15ff1f1f9c1306a74ab4e32e6
waney <ssdii66@gmail.com>
2025-08-02 21:25:18 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import {ref} from "vue";
+
+let isModal = ref(true);
+
+
+</script>
+
+<template>
+<div>
+  <h1>vue3 job</h1>
+</div>
+
+
+</template>
+
+<style scoped>
+
+
+</style>
