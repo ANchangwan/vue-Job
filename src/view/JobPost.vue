4be0009183db650b4cec6ddb343d0c98a8ffddbc
waney <ssdii66@gmail.com>
2025-08-10 23:00:54 +0900

    radio

--- src/view/JobPost.vue
+++ src/view/JobPost.vue
@@ -1,11 +1,132 @@
 <script setup>
+import {ref} from "vue";
+
+const title = ref('');
+const todo = ref('');
+const pay_rule = ref('');
+const pay = ref("");
+
 
 </script>
 
 <template>
-  <h1>Jobpost</h1>
+  <div class="form-container">
+    <div class="form-group">
+      <label for="title">제목</label>
+      <input
+          type="text"
+          id="title"
+          required
+          placeholder="공고 내용을 요약해 주세요."
+          v-model="title"
+      />
+    </div>
+    <div class="form-group">
+      <label for="todo">하는 일</label>
+      <input
+        type="text"
+        id="todo"
+        v-model="todo"
+        placeholder="해야할 업무를 입력해주세요.s"
+      />
+    </div>
+<!--    급여조건-->
+    <div class="form-group">
+      <input
+          type="radio"
+          id="pay_rule1"
+          v-model="pay_rule"
+          value="시급"
+          name="pay_rule"
+          required
+          checked
+      />
+      <input
+          type="radio"
+          id="pay_rule2"
+          v-model="pay_rule"
+          value="월급"
+          name="pay_rule"
+          required
+          checked
+      />
+      <div class="tab-group">
+        <label for="pay_rule1">시급</label>
+        <label for="pay_rule2">월급</label>
+      </div>
+    </div>
+    <input
+        type="number"
+        id="pay"
+        v-model="pay"
+        required
+        placeholder="시급 또는 월급을 입력해주세요!"
+    />
+  </div>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+@import url("../styles/form.scss");
 
+.form-container {
+  margin-top: 20px;
+  padding-bottom: 50px;
+
+  .tab-group {
+    display: flex;
+    gap: 15px;
+    label {
+      flex: 1;
+      border: 1px solid var(--main-color-dark);
+      border-radius: 8px;
+      text-align: center;
+      padding: 12px;
+    }
+  }
+
+  input[type="radio"] {
+    display: none;
+  }
+
+  input[type="radio"]:nth-child(1):checked ~ .tab-group label:nth-child(1) {
+    background: var(--main-color-dark);
+    color: #fff;
+  }
+
+  input[type="radio"]:nth-child(2):checked ~ .tab-group label:nth-child(2) {
+    background: var(--main-color-dark);
+    color: #fff;
+  }
+
+  #pay { margin-top: 8px;}
+
+  // for=photo를 가진 form-group의 후손 input의 보더 스타일 제거
+  .form-group:has(label[for=photo]) input {
+    border: none;
+  }
+
+  //file 아이콘
+  label[for=photo] {
+    figure {
+      display: flex;
+      align-items: center;
+      img {
+        border: 1px solid red;
+        margin-left: 30px;
+      }
+    }
+  }
+  input[type="file"] {
+    display: none;
+  }
+}
+
+.btn-submit {
+  background: var(--main-color-light);
+}
+
+.form-group:has(label[for=photo]) {
+  padding-bottom: 25px;
+  border-bottom: 5px solid #ccc;
+}
 </style>
--- src/view/UserProfile.vue
+++ src/view/UserProfile.vue
@@ -1,3 +1,4 @@
+
 <script setup>
 import supabase from "../supabase.js";
 import {onMounted, ref} from "vue";
@@ -7,13 +8,28 @@ import {useRouter} from "vue-router";
 const router = useRouter();
 const isLogin = ref(false); // 화면 표시 상태 변수
 
+const name = ref('');
+const addr = ref('사용자 주소');
+const text = ref("IT와 웹 개발에 대한 열정을 가진 개발자입니다.")
+
 onMounted(async () => {
   const { data: { user } } = await supabase.auth.getUser();
-  console.log(user.email);
 
   if (user){
     console.log("로그인");
     isLogin.value = true;
+    // user 정보 가져오기
+    const { data, error } = await supabase
+        .from('user_table')
+        .select()
+        .eq('id', user.id)
+    if (data){
+      name.value = data[0].name;
+      addr.value = data[0].address;
+      text.value = data[0].text;
+    }else {
+      console.log(data);
+    }
   }else {
     alert("로그인 후 사용해주세요!");
     isLogin.value = false;
@@ -33,9 +49,19 @@ const handleLogOut = async () => {
 
 <template>
   <div class="container" v-if="isLogin">
-    <div class="form-container">
-      <button @click="handleLogOut">로그아웃</button>
+    <div class="top-info-box">
+      <img src="/vite.svg" alt="profile">
+      <div class="right-info">
+        <span class="name">{{ name }}</span>
+        <address>{{ addr }}</address>
+      </div>
     </div>
+    <div class="text-info">
+      <h4 class="text-info__title">자기소개</h4>
+      <p class="text-info__introduce">{{ text }}</p>
+    </div>
+    <button @click="handleLogOut">로그아웃</button>
+
   </div>
 
 </template>
@@ -52,4 +78,27 @@ button{
     text-decoration: underline;
   }
 }
+.top-info-box{
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  margin-bottom: 20px;
+}
+.text-info{
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  .text-info__title{
+    color: var(--main-color-dark);
+    font-weight: bold;
+    font-size: 18px;
+  }
+  .text-info__introduce{
+    padding : 15px;
+    border-radius: 5px;
+    border : 1px solid var(--main-color-dark);
+    width: 100%;
+  }
+}
+
 </style>
